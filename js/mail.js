09e5cff1c556109eb741a986654a8ff8a8694e63
PrathameshMalavi <114830029+PrathameshMalavi@users.noreply.github.com>
2024-05-02 14:08:24 +0530

    Update mail.js

--- js/mail.js
+++ js/mail.js
@@ -1,19 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBJbZtH1u7IMNKl7eztBfwwZNVp2B76UEQ",
-
-  authDomain: "news-app-16978.firebaseapp.com",
-
-  databaseURL: "https://news-app-16978-default-rtdb.firebaseio.com",
-
-  projectId: "news-app-16978",
-
-  storageBucket: "news-app-16978.appspot.com",
-
-  messagingSenderId: "570595830203",
-
-  appId: "1:570595830203:web:7526c2d68ad73cd22e7c9e",
-
-  measurementId: "G-7PLDL7TQVR",
+  //get from your firebase project
 };
 
 //initialize Firebase
